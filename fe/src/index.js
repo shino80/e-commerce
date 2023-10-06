38999b39929c69fea73b33355ad75e4543da58b5
will <will@MacBook-Pro-cua-will.local>
2023-10-06 10:14:58 +0900

    udpate

--- fe/src/index.js
+++ fe/src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 <Provider store={store}>
-    <BrowserRouter basename='/'>
+    <BrowserRouter >
       {" "}
       <PersistGate loading={null} persistor={persistor}>
       <App />
