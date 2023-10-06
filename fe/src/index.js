eeb1653b88dfc307c4dc2b159f050abdaa09aebb
will <will@MacBook-Pro-cua-will.local>
2023-10-06 11:33:22 +0900

    update

--- fe/src/index.js
+++ fe/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from 'react-redux'
+import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { persistor } from "./redux/store";
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<Provider store={store}>
-    <BrowserRouter >
+  <BrowserRouter basename="/">
+    <Provider store={store}>
       {" "}
       <PersistGate loading={null} persistor={persistor}>
-      <App />
-     </PersistGate>
-    </BrowserRouter>
+        <App />
+      </PersistGate>
     </Provider>
+  </BrowserRouter>
 );
