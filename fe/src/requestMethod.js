199ce96282161087c5abbe36cc92a74748a698b8
will <will@MacBook-Pro-cua-will.local>
2023-10-06 22:59:49 +0900

    update

--- fe/src/requestMethod.js
+++ fe/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api/";
+const BASE_URL = "https://goraku-shop.onrender.com/api/";
 // const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 // const currentUser = user && JSON.parse(user).currentUser;
 // const TOKEN = currentUser?.accessToken;
