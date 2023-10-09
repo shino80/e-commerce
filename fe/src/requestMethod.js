8fadff13663d50cfc7ba10d1fbbcb1b9b61b2009
will <will@MacBook-Pro-cua-will.local>
2023-10-09 22:36:05 +0900

    update

--- fe/src/requestMethod.js
+++ fe/src/requestMethod.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "https://goraku-shop.onrender.com/api/";
-// const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
-// const currentUser = user && JSON.parse(user).currentUser;
-// const TOKEN = currentUser?.accessToken;
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  ?.currentUser?.accessToken;
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicReq = axios.create({
   baseURL: BASE_URL,
