bd98cd0a706867e7c5f01b22431dc1719f6c7cae
will <will@MacBook-Pro-cua-will.local>
2024-05-23 18:45:15 +0900

    change api

--- fe/src/requestMethod.js
+++ fe/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://tame-ruby-narwhal-robe.cyclic.app/api/";
+const BASE_URL = "https://goraku-blog.onrender.com/api";
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
